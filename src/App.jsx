8b3ba19e06a9f129873de07b44975a89be8f6897
Kenediii <156183790+Kenediii@users.noreply.github.com>
2024-05-25 18:21:14 +0300

    fx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,50 @@
-
-
+import Description from "./components/Description/Description"
+import Options from "./components/Options/Options"
+import Feedback from "./components/Feedback/Feedback";
+import Notification from "./components/Notification/Notification";
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const initialState = { good: 0, neutral: 0, bad: 0 };
+  const [points, setPoints] = useState(() => {
+    const savedPoints = window.localStorage.getItem("points");
+    return savedPoints ? JSON.parse(savedPoints) : initialState;
+  });
 
+  useEffect(() => {
+    window.localStorage.setItem("points", JSON.stringify(points));
+  }, [points]);
 
-  return <>
+  const updateFeedback = (type) => {
+    if (type === "reset") {
+      setPoints({ good: 0, neutral: 0, bad: 0 });
+      return;
+    }
+    setPoints((prev) => ({
+      ...prev,
+      [type]: prev[type] + 1,
+    }));
+  };
 
-  </>
-}
+  const totalFeedback = points.good + points.neutral + points.bad;
 
-export default App
+  const positiveFeedback = Math.round((points.good / totalFeedback) * 100);
+
+  return (
+    <>
+      <Description />
+      <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} />
+      {totalFeedback > 0 ? (
+        <Feedback
+          points={points}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      ) : (
+        <Notification message="No feedback yet" />
+      )}
+    </>
+  );
+};
+
+export default App;
